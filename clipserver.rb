6d2754fa5b86efe7c0fc7293beaf334f054b90fc
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2016-11-09 19:35:15 +0900

    first!

---
+++ clipserver.rb
@@ -0,0 +1,15 @@
+class Clipserver < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/tzmfreedom/clipserver.git"
+  sha256 ""
+  version "0.0.1"
+
+  def install
+    bin.install "bin/clipserver"
+  end
+
+  test do
+    system "false"
+  end
+end
